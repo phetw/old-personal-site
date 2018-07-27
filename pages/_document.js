1d3ff3866559fdfd2440c641684816c18f3951c1
was.li <wasuwat.se@gmail.com>
2018-07-27 18:12:26 +0700

    [🐛] fix typo

--- pages/_document.js
+++ pages/_document.js
@@ -18,12 +18,15 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width" />
           <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:100,400" rel="stylesheet" />
           <meta key="twitter-card" name="twitter:card" content="summary" />
-          <meta key="twitter-site" name="twitter:site" content="@ianmitchel1" />
+          <meta key="twitter-site" name="twitter:site" content="@was_mee" />
           <meta key="twitter-description" name="twitter:description" />
           <meta key="twitter-image" name="twitter:image" content="https://picsum.photos/g/1200/630?image=1045" />
+          <meta key="twitter-description" name="twitter:description" content="" />
           <meta key="facebook-type" property="og:type" content="article" />
-          <meta key="facebook-description" property="og:description" />
+          <meta key="facebook-title" property="og:title" content="Hi there 🙌" />
+          <meta key="facebook-description" property="og:description" content="" />
           <meta key="facebook-image" property="og:image" content="https://picsum.photos/g/1200/630?image=1045" />
+          <meta key="facebook-url" property="og:url" content="https://rappad.github.io/wasuwat/" />
           {this.props.styleTags}
         </Head>
         <body>
